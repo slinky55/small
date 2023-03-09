06ec2b541ae2ac4393ec8ad84b680bbc86783404
slinky55 <larry.mason@ufl.edu>
2023-03-09 15:09:10 -0500

    added ping route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,30 @@
+use actix_web::{
+    get,
+    post,
+    web,
+    App,
+    HttpResponse,
+    HttpServer,
+    Responder
+};
 
+// use entity::{
+//     user::*,
+//     post::*
+// };
 
-fn main() {
-    println!("Hello, world!");
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(ping)
+    })
+        .bind(("127.0.0.1", 7100))?
+        .run()
+        .await
 }
