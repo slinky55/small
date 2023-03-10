72b4e16a7dfc0976d448e9aa2343ee9916ad9d31
slinky55 <larry.mason@ufl.edu>
2023-03-10 02:15:22 -0500

    updated backend routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,21 +8,26 @@ use actix_web::{
     Responder
 };
 
-// use entity::{
-//     user::*,
-//     post::*
-// };
-
 #[get("/ping")]
 async fn ping() -> impl Responder {
+    println!("GET /api/ping 200");
     HttpResponse::Ok().body("pong")
+}
+
+#[post("/user/create")]
+async fn user_create() -> impl Responder {
+    println!("POST /api/user/create 200");
+    HttpResponse::Ok().body("user created")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(ping)
+            .service(
+                web::scope("/api")
+                .service(ping)
+            )
     })
         .bind(("127.0.0.1", 7100))?
         .run()
